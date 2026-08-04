095312488e986718547b7aa1b45b006821b835bd
joice003 <joicemonteirodasilva@gmail.com>
2026-08-03 21:29:52 -0300

    Aula do dia 03/08/2026.

---
+++ Parte02-Colecoes/01_lista/main.py
@@ -0,0 +1,11 @@
+# lista de nomes (usa [] sempre)
+nomes = ['Joice', 'Ana', 'Maria', 'João', 'Paulo']
+
+# exibe os elementos da lista
+print(nomes[0])
+print(nomes[3])
+print(nomes)
+
+# exibe toda a lista de forma ordenada, diferente do código anterior q mostra da forma que escrevi
+for nome in nomes:
+    print(nome)
---
+++ Parte02-Colecoes/02_lista_numerada/main.py
@@ -0,0 +1,5 @@
+frutas = ['Maça','Abacaxi', 'Morango','Maracujá']
+
+# enumerar a lista
+for i,fruta in enumerate(frutas, start=1):
+    print(f'Fruta {i}: {fruta}')
