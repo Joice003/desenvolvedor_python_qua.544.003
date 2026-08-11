73c2c0dcbcd6f613c7b6a0ea0a7f6527934dff3d
joice003 <joicemonteirodasilva@gmail.com>
2026-08-10 21:39:29 -0300

    Aula do dia 10/08/2026.

---
+++ Parte02-Colecoes/22_range/main.py
@@ -0,0 +1,5 @@
+nomes = ["Fulano", "Cicrano", "Beltrano", "João", "Maira", "Jose"]
+
+# range determina quanto será apresentado, no caso range 4 vai apresentar só os 4 primeiros nomes
+for i in range(4):
+    print(nomes[i])
